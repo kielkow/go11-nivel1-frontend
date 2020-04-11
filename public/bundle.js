8e9e21c5b1a32e019262d462779e4d113bbbf7f0
kielkow <matheuskiel@fiorifer.com.br>
2020-04-11 13:31:07 -0300

    Config Babel

---
+++ public/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var sum = function sum(a, b) {
+  return a + b;
+};
+
+console.log('SUM', sum(1, 5));
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const sum = (a, b) => a + b;
+
+console.log('SUM', sum(1, 5));
