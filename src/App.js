8dfd43e8e6a13e7e34bf9998a013996977a5ad90
kielkow <matheuskiel@fiorifer.com.br>
2020-04-11 17:51:12 -0300

    Props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Menu</li>
+          <li>Projects</li>
+          <li>Profile</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>ReactJS</li>
+          <li>Node.js</li>
+          <li>React Native</li>
+      </ul>
+      </Header>
     </>
   );
 }
