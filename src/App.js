21185c0a060bfd3521cbfe1a9a261bbec8297ae1
kielkow <matheuskiel@fiorifer.com.br>
2020-04-11 22:32:58 -0300

    List projects from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import background from './assets/photo.jpg';
 
 import Header from './components/Header';
 
 function App() {
   const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => setProjects(response.data));
+  }, []);
 
   const handleAddProject = () => {
     setProjects([...projects, `New project ${Date.now()}`]);
@@ -16,10 +20,8 @@ function App() {
     <>
       <Header title="Homepage" />
 
-      <img width={300} src={background} alt="background"/>
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Add</button>
